d63b0b34e4906aa7a48fc5efa501a9ecac9d5f28
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-11-12 07:55:04 -0600

    props values

--- part-2/App.js
+++ part-2/App.js
@@ -1,7 +1,7 @@
 const App = () => (
     <div>
-      <Tweet username="" name="" date="" message="" />
-      <Tweet username="" name="" date="" message="" />
-      <Tweet username="" name="" date="" message="" />
+      <Tweet username="happy321" name="Happy" date="{new Date().toDateString()}" message="Fake Tweet!" />
+      <Tweet username="yes300" name="Yes" date="new Date().toDateString()" message="Testing Components" />
+      <Tweet username="bestever99" name="Best" date="new Date().toDateString()" message="Tweeting on Twitter Every Day" />
     </div>
 )
