c61f7e3bfd7301c61dc85b8daca68874bd521f3a
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-11-12 07:41:11 -0600

    Component functions and add props.name to App.js

--- part-1/App.js
+++ part-1/App.js
@@ -1,7 +1,7 @@
 const App = () => (
     <div>
       <FirstComponent />
-      <NamedComponent />
+      <NamedComponent name="Taylor" />
     </div>
 )
   
---
+++ part-1/NamedComponent.js
@@ -0,0 +1,7 @@
+const NamedComponent = (props) => {
+    return (
+        <div>
+            <p>My name is {props.name}</p>
+        </div>
+    );
+}
