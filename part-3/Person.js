a51b19c830c93b0c4b1158842be58399e269d091
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-11-12 08:12:02 -0600

    slice name

--- part-3/Person.js
+++ part-3/Person.js
@@ -1,9 +1,10 @@
 const Person = (props) => {
+    let voteMsg = props.age >= 18 ? "please go vote!" : "you must be 18";
     return (
         <div>
             <p>Learn some information about this person</p>
-            <h3>{props.age}: {props.age > 18 ? "please go vote!" : "you must be 18"}</h3>
-            <h3>{props.name.length}</h3>
+            <h3>{props.age}: {voteMsg}</h3>
+            <h3>{props.name.slice(0, 6)}</h3>
             <h3>Hobbies:</h3>
             <ul>
                 {props.hobbies.map(h => <li>{h}</li>)}
